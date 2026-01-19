d9dd3528470d0a044480c1a3e4f4ba39959e351f
maybecocheon <guswl293@gmail.com>
2026-01-19 16:13:13 +0900

    callback update

--- medical_FE/src/app/callback/page.tsx
+++ medical_FE/src/app/callback/page.tsx
@@ -6,50 +6,42 @@ export default function OAuth2Callback() {
     const router = useRouter();
 
     useEffect(() => {
-        const fetchCallback = async () => {
-            try {
-                const response = await fetch("https://nonefficient-lezlie-progressively.ngrok-free.dev/api/jwtcallback", {
-                    method: "POST",
-                    credentials: "include", // 쿠키포함
-                });
-                if (response.ok) {
-                    const params = new URLSearchParams(window.location.search);
-                    const username = params.get("username")
+        const params = new URLSearchParams(window.location.search);
+        const token = params.get("token"); // URL에서 token 추출
+        const username = params.get("username");
 
-                    const jwtToken = response.headers.get('Authorization');
+        if (token && username) {
+            sessionStorage.setItem("jwtToken", "Bearer " + token);
+            sessionStorage.setItem("username", username);
+            sessionStorage.setItem("role", "ROLE_MEMBER");
 
-                    const resp = await fetch(`https://nonefficient-lezlie-progressively.ngrok-free.dev/api/getMember/${username}`)
-                    const data = await resp.json()
-                    const alias = data.alias
+            const fetchCallback = async () => {
 
-                    if (jwtToken) {
-                        sessionStorage.setItem("jwtToken", jwtToken);
-                        sessionStorage.setItem('username', username!);
-                        sessionStorage.setItem("role", "ROLE_MEMBER");
-                        sessionStorage.setItem('alias', alias!);
-                    }
-                    alert("로그인 성공!");
-                    // 로그인 성공 로직 내부
-                    const redirectUrl = sessionStorage.getItem('redirectUrl');
+                const resp = await fetch(`https://nonefficient-lezlie-progressively.ngrok-free.dev/api/getMember/${username}`)
+                const data = await resp.json()
+                const alias = data.alias;
 
-                    if (redirectUrl) {
-                        // 저장된 URL이 있으면 해당 페이지로 이동 후 데이터 삭제
-                        sessionStorage.removeItem('redirectUrl');
-                        router.push(redirectUrl);
-                    } else {
-                        // 저장된 URL이 없으면 기본 페이지(예: 메인)로 이동
-                        router.push('/medicalInfo');
-                    }
+                sessionStorage.setItem('alias', alias!);
+
+                alert("로그인 성공!");
+
+                // 로그인 성공 로직 내부
+                const redirectUrl = sessionStorage.getItem('redirectUrl');
+
+                if (redirectUrl) {
+                    // 저장된 URL이 있으면 해당 페이지로 이동 후 데이터 삭제
+                    sessionStorage.removeItem('redirectUrl');
+                    router.push(redirectUrl);
                 } else {
-                    alert("JWT 검증 실패");
-                    router.push("/");
+                    // 저장된 URL이 없으면 기본 페이지(예: 메인)로 이동
+                    router.push('/medicalInfo');
                 }
-            } catch (err) {
-                alert("서버 요청 오류");
-                router.push("/");
-            }
-        };
-        fetchCallback();
+            };
+            fetchCallback();
+        } else {
+        alert("인증 정보가 없습니다.");
+        router.push("/");
+    }
     }, []);
     return <p>로그인 처리 중...</p>;
 };
