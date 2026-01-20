e3bea93f1d3d193e86d44054e79784865b84daf1
maybecocheon <guswl293@gmail.com>
2026-01-20 11:53:44 +0900

    oauth2 test

--- medical_FE/src/app/callback/page.tsx
+++ medical_FE/src/app/callback/page.tsx
@@ -6,24 +6,28 @@ export default function OAuth2Callback() {
     const router = useRouter();
 
     useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
-        const username = params.get("username");
-        const jwtToken = params.get("jwtToken");
-
-        if (jwtToken && username) {
-            sessionStorage.setItem("jwtToken", jwtToken);
-            sessionStorage.setItem('username', username);
-            sessionStorage.setItem("role", "ROLE_MEMBER");
-        }
-
         const fetchCallback = async () => {
             try {
-                const response = await fetch(`/proxy/api/getMember/${username}`);
-                const data = await response.json();
-                const alias = data.alias;
-                sessionStorage.setItem('alias', alias!);
+                const response = await fetch("/proxy/api/jwtcallback", {
+                    method: "POST",
+                    credentials: "include", // 쿠키포함
+                });
+                if (response.ok) {
+                    const params = new URLSearchParams(window.location.search);
+                    const username = params.get("username")
 
+                    const jwtToken = response.headers.get('Authorization');
 
+                    const resp = await fetch(`/proxy/api/getMember/${username}`)
+                    const data = await resp.json()
+                    const alias = data.alias
+
+                    if (jwtToken) {
+                        sessionStorage.setItem("jwtToken", jwtToken);
+                        sessionStorage.setItem('username', username!);
+                        sessionStorage.setItem("role", "ROLE_MEMBER");
+                        sessionStorage.setItem('alias', alias!);
+                    }
                     alert("로그인 성공!");
                     // 로그인 성공 로직 내부
                     const redirectUrl = sessionStorage.getItem('redirectUrl');
@@ -36,6 +40,10 @@ export default function OAuth2Callback() {
                         // 저장된 URL이 없으면 기본 페이지(예: 메인)로 이동
                         router.push('/medicalInfo');
                     }
+                } else {
+                    alert("JWT 검증 실패");
+                    router.push("/");
+                }
             } catch (err) {
                 alert("서버 요청 오류");
                 router.push("/");
